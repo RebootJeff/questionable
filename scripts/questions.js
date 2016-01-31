8771046c7e92c3b03fd36230b654d955345a06be
Jeff Lee <RebootJeff@users.noreply.github.com>
2016-01-30 17:40:34 -0800

    Add one more placeholder question

--- scripts/questions.js
+++ scripts/questions.js
@@ -10,8 +10,18 @@ const questions = [
     maxDates: Infinity
   },
   {
-    category: 'Friends and Family',
+    category: 'Personal History',
     text: 'example question text 2',
+    exampleAnswers: [
+      'example answer 1',
+      'example answer 2'
+    ],
+    minDates: 0,
+    maxDates: Infinity
+  },
+  {
+    category: 'Dating',
+    text: 'example question text 3',
     exampleAnswers: [
       'example answer 1',
       'example answer 2'
